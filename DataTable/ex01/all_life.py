7baa0cea5011c6b37ca4edc2e08985ac02d435db
Antoine Mouly <amouly@k0r3p3.42mulhouse.fr>
2024-04-14 12:18:46 +0200

    datatable continue

--- DataTable/ex01/all_life.py
+++ DataTable/ex01/all_life.py
@@ -1,11 +1,20 @@
 from load_csv import load
 import matplotlib.pyplot as plt
+import pandas as pd
 import seaborn as sns
 
-df = load("life_expectancy_years.csv")
+dataset = load("life_expectancy_years.csv")
+germany_data = dataset[dataset['country'] == 'Germany']
+germany_data.head()
+years = germany_data.columns[1:]
+life_expectancy = germany_data.values[0][1:]
 
-df = df.transpose()
-df.columns = df.iloc[0]
-df.head()
-df = df[1:]
-df.head()
+plt.plot(years, life_expectancy, label='France')
+plt.title('Life Expectancy in France Over the Years')
+plt.xlabel('Year')
+plt.xticks(years[::40], rotation=45)
+plt.ylabel('Life Expectancy')
+plt.yticks(range(30, 101, 10))
+plt.legend()
+plt.tight_layout()
+plt.show()

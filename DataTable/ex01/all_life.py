16c4ca3e3485b8d5964b00440f9240b5cecd90c2
Antoine Mouly <amouly@k0r3p3.42mulhouse.fr>
2024-04-15 10:27:55 +0200

    datatable continue

--- DataTable/ex01/all_life.py
+++ DataTable/ex01/all_life.py
@@ -1,20 +1,28 @@
 from load_csv import load
 import matplotlib.pyplot as plt
-import pandas as pd
-import seaborn as sns
 
-dataset = load("life_expectancy_years.csv")
-germany_data = dataset[dataset['country'] == 'Germany']
-germany_data.head()
-years = germany_data.columns[1:]
-life_expectancy = germany_data.values[0][1:]
+def main():
+    dataset = load("life_expectancy_years.csv")
+    france_data = dataset[dataset['country'] == 'France']
+    france_data_transposed = france_data.transpose()
+    #drop the first row
+    france_data = france_data_transposed[1:].copy()
+    france_data.columns = france_data_transposed.iloc[0]
+    #rename the first column
+    france_data.rename(columns={france_data.columns[0]: 'Expectancy'}, inplace=True)
+    france_data.reset_index(inplace=True)
+    france_data.rename(columns={'index': 'Year'}, inplace=True)
 
-plt.plot(years, life_expectancy, label='France')
-plt.title('Life Expectancy in France Over the Years')
-plt.xlabel('Year')
-plt.xticks(years[::40], rotation=45)
-plt.ylabel('Life Expectancy')
-plt.yticks(range(30, 101, 10))
-plt.legend()
-plt.tight_layout()
-plt.show()
+
+
+    plt.plot(france_data['Year'].values, france_data['Expectancy'].values, label='France')
+    plt.title('France Life Expectancy Projections')
+    plt.xlabel('Year')
+    plt.xticks(france_data['Year'].values[::40], rotation=45)
+    plt.ylabel('Life Expectancy')
+    plt.yticks(range(30, 101, 10))
+    # plt.tight_layout()
+    plt.show()
+
+if __name__ == "__main__":
+    main()

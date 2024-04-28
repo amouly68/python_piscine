32fd339f9ea561c299adc36493b2b7216043f655
Antoine Mouly <amouly@k0r3p4.42mulhouse.fr>
2024-04-28 13:45:01 +0200

    fin array

---
+++ Array/ex05/load_image.py
@@ -0,0 +1,28 @@
+from PIL import Image
+import numpy as np
+
+def ft_load(path):
+    """
+    Load an image and print its format and pixel content in RGB format.
+
+    Args:
+    path (str): Path to the image file.
+
+    Returns:
+    numpy.ndarray: Array representing the pixel content of the image.
+    """
+    try:
+        img = Image.open(path)
+
+        # print(f"The shape of image is: ({img.size[1]}, {img.size[0]}, {img.layers})")
+
+        
+        img_array = np.array(img)
+        # print(f"The shape of image is: {img_array.shape}" )
+
+        return img_array
+    except Exception as e:
+        print(f"Error loading image: {e}")
+        return None
+
+

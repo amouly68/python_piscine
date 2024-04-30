f8471a47eee0d01ce4fe6094165069e34c89f71b
Antoine Mouly <amouly@k0r3p4.42mulhouse.fr>
2024-04-30 13:46:53 +0200

    Dod fini norme

--- Dod/ex03/new_student.py
+++ Dod/ex03/new_student.py
@@ -2,9 +2,18 @@ import random
 import string
 from dataclasses import dataclass, field
 
+
 def generate_id() -> str:
-    return "".join(random.choices(string.ascii_lowercase, k = 15))
+    return "".join(random.choices(string.ascii_lowercase, k=15))
+
 
 @dataclass
 class Student:
-    pass
+    name: str
+    surname: str
+    active: bool = field(default=True, init=False)
+    login: str = field(init=False)
+    id: str = field(default_factory=generate_id, init=False)
+
+    def __post_init__(self):
+        self.login = (self.name[0] + self.surname).lower()
--- Dod/ex03/tester.py
+++ Dod/ex03/tester.py
@@ -2,10 +2,15 @@ from new_student import Student
 
 
 try:
-    student = Student(name = "Edward", surname = "agle")
+    student = Student(name="Edward", surname="agle")
     print(student)
+    print(student.__dict__)
 except Exception as e:
-    print (e)
+    print(e)
 
-student = Student(name = "Edward", surname = "agle", id = "toto")
-print(student)
+try:
+    student = Student(name="Edward", surname="agle", id="toto")
+    print(student)
+    print(student.__dict__)
+except Exception as e:
+    print(e)
